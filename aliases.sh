f497b033edb293ff500352a0cc532b6b6feab4bc
Kim Hunter <bigkm1@gmail.com>
2013-10-24 22:20:20 +0100

    Remove unused functions and aliases

--- aliases.sh
+++ aliases.sh
@@ -1,11 +1,8 @@
 alias ls='ls -G'
-alias ll='ls -lh'
 alias la='ls -lah'
 alias ..='cd ..;'
 alias ...='.. ..'
-alias ducks='du -cks * | sort -rn|head -11' # Lists the size of all the folders$
 alias top='top -o cpu'
-alias systail='tail -f /var/log/system.log'
 alias du1="du  -hd1"
 alias mate="mate -r"
 alias vi="vim"
@@ -22,32 +19,17 @@ alias cff="clang-format -style=file"
 alias fjson="python -mjson.tool"
 alias safari_curl='curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/534.57.2 (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2"'
 
-# = Kit Related =
-alias k="kit"
-alias ku="rm -rdf build Kits ~/.kit/packages/*; kit update"
-alias clear-kit="mv ~/.kit/packages ~/.Trash/"
-
 alias qlp="qlmanage -p"
 alias s="screen"
 alias dfh="df -h"
 alias xcwhich="xcode-select -print-path; xcodebuild -version"
 
-# remove Derived Data
 alias xcrmdd="rm -rdf ~/NoBackup/Xcode"
 alias xcb="xcodebuild -jobs 8"
 alias xcb-debug="time xcodebuild -jobs 8 -configuration Debug"
 alias goo="go art"
 alias fuckingnetwork="networksetup -setairportpower  en1 off ; sleep 3; networksetup -setairportpower en1 on"
 
-function editprofile() {
-	if [[ $# -gt 0 ]]; then
-		mate -w $1 
-		. ~/.bash_profile
-	else	
-		echo "Please Supply a file name"
-		return -1
-	fi
-}
 function repeter() {
     yes "$*" | xargs say
 }
@@ -59,41 +41,7 @@ function gtow() {
   go
   git clone $1 $2 && gittower $2
 }
-removeduplines() {
-awk '{
-if ($0 in stored_lines)
-   x=1
-else
-   print
-   stored_lines[$0]=1
-}' $1  
-}
 
-# Symlink kit as a dev-package
-function link-kit() {
-	PROJECT_DIRS="$HOME/Projects"
-	DPK="dev-packages"
-	if [[ -f "KitSpec" ]]; then		
-		RES=`find $PROJECT_DIRS -iname "${1}*" -maxdepth 1 | head -n 1`
-		if [[ "$RES" == "" ]]; then
-			echo  "Couldn't find '$1' in $HOME/Projects/"
-			return 1
-		elif [[ -f "$RES/KitSpec" ]]; then
-			mkdir -p $DPK
- 			cd $DPK
-			ln -s $RES $(basename $RES)
-			cd ..
-			echo "Linked Package $RES"
-            kit up
-			return 0
-		else
-			echo "$RES isn't a kit"
-		fi
-	else
-		echo "$(pwd) isn't a kit"
-		return 1
-	fi
-}
 function h() {
   hoogle --color --count=30 ${1} 
 }
@@ -161,12 +109,6 @@ function go () {
 	else
 		cd "$RES"
 	fi
-}
-
-function gob() {
-    cwd=$PWD
-    for d in viewer oompf-ipad; do go $d; git co $1 ; done
-	cd "$cwd"
 }
 
 
